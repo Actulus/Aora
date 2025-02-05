0f12a2c2ca174146c7bbad170d3b10b17bcaa868
Schifler Patricia-Vivien <93488786+Actulus@users.noreply.github.com>
2025-02-05 20:23:30 +0200

    feat: add initial project setup

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,9 +6,9 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/explore` | `/(tabs)\` | `/(tabs)\_layout` | `/(tabs)\explore` | `/..\components\Collapsible` | `/..\components\ExternalLink` | `/..\components\HapticTab` | `/..\components\HelloWave` | `/..\components\ParallaxScrollView` | `/..\components\ThemedText` | `/..\components\ThemedView` | `/..\components\ui\IconSymbol` | `/..\components\ui\IconSymbol.ios` | `/..\components\ui\TabBarBackground.ios` | `/..\constants\Colors` | `/..\hooks\useColorScheme` | `/..\hooks\useColorScheme.web` | `/..\hooks\useThemeColor` | `/_sitemap` | `/explore`;
-      DynamicRoutes: never;
-      DynamicRouteTemplate: never;
+      StaticRoutes: `/` | `/(auth)` | `/(auth)/sign-in` | `/(auth)/sign-up` | `/(tabs)` | `/(tabs)/` | `/(tabs)/bookmark` | `/(tabs)/create` | `/(tabs)/explore` | `/(tabs)/home` | `/(tabs)/profile` | `/..\components\CustomButton` | `/_sitemap` | `/bookmark` | `/create` | `/explore` | `/home` | `/profile` | `/sign-in` | `/sign-up`;
+      DynamicRoutes: `/${Router.SingleRoutePart<T>}` | `/(search)/${Router.SingleRoutePart<T>}`;
+      DynamicRouteTemplate: `/(search)/[query]` | `/[query]`;
     }
   }
 }
