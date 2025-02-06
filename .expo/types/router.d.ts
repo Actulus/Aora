ac9c0463df1556a21eaa618d7597b1d5752d7962
Schifler Patricia-Vivien <93488786+Actulus@users.noreply.github.com>
2025-02-06 13:32:56 +0200

    feat: extended configs

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(auth)` | `/(auth)/sign-in` | `/(auth)/sign-up` | `/(tabs)` | `/(tabs)/` | `/(tabs)/bookmark` | `/(tabs)/create` | `/(tabs)/explore` | `/(tabs)/home` | `/(tabs)/profile` | `/..\components\CustomButton` | `/_sitemap` | `/bookmark` | `/create` | `/explore` | `/home` | `/profile` | `/sign-in` | `/sign-up`;
+      StaticRoutes: `/` | `/(auth)` | `/(auth)/sign-in` | `/(auth)/sign-up` | `/(tabs)` | `/(tabs)/bookmark` | `/(tabs)/create` | `/(tabs)/home` | `/(tabs)/profile` | `/_sitemap` | `/bookmark` | `/create` | `/home` | `/profile` | `/sign-in` | `/sign-up`;
       DynamicRoutes: `/${Router.SingleRoutePart<T>}` | `/(search)/${Router.SingleRoutePart<T>}`;
       DynamicRouteTemplate: `/(search)/[query]` | `/[query]`;
     }
